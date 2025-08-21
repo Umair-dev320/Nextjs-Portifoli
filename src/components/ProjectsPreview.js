52652be371b2783a42e3f1b29feb93619a1eacf2
Muhammad Umair <141720029+Umair-dev320@users.noreply.github.com>
2025-08-21 14:56:10 +0500

    update the ui
    update

--- src/components/ProjectsPreview.js
+++ src/components/ProjectsPreview.js
@@ -5,25 +5,25 @@ import Image from "next/image";
 export default function ProjectsPreview() {
   const projects = [
     {
+      title: "Khub.biz website",
+      description:
+        "Redesigned and developed pages for Khub.biz using Figma prototypes, focusing on clean, responsive layouts with HTML, CSS, and JavaScript.",
+      image: "/images/projects/Khub.png",
+      link: "https://khub.biz/",
+    },
+    {
+      title: "ititans.com website",
+      description:
+        "Developed and redesigned pages for ititans.com using Figma prototypes, focusing on responsive and user-friendly design with HTML, CSS, and JavaScript.",
+      image: "/images/projects/ititans.png",
+      link: "https://ititans.com/",
+    },
+    {
       title: "Food Delivery App",
       description:
-        "A React-based food ordering app with Firebase and real-time cart updates.",
+        "A React-based food ordering app powered by Firebase, featuring categorized menus, user authentication, and real-time cart updates a seamless ordering experience.",
       image: "/images/projects/Omnifood-img.png",
       link: "https://ordernow-xi.vercel.app/",
-    },
-    {
-      title: "MD-Notes Website",
-      description:
-        "A personal daily notes application built with Next.js and Firebase.",
-      image: "/images/projects/MD-Notes-img.png",
-      link: "https://nestjs-15-muhammadumair160s-projects.vercel.app/",
-    },
-    {
-      title: "E-Commerce Dashboard",
-      description:
-        "Admin dashboard for managing products, users, and orders using Next.js.",
-      image: "/images/projects/quick-card.png",
-      link: "https://letfix-zeta.vercel.app/",
     },
   ];
 
@@ -42,13 +42,19 @@ export default function ProjectsPreview() {
               key={index}
               className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow"
             >
-              <Image
-                src={project.image}
-                alt={project.title}
-                width={500}
-                height={300}
-                className="w-full h-48 object-cover"
-              />
+              <Link
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  width={500}
+                  height={300}
+                  className="w-full h-48 object-cover cursor-pointer"
+                />
+              </Link>
               <div className="p-4 text-left">
                 <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
                 <p className="text-gray-600 dark:text-gray-300 mb-3 text-sm">
